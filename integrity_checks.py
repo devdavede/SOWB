a0e9d969c59fab829c0033d9f520624d9f1ededb
David <55126143+devdavede@users.noreply.github.com>
2024-03-22 01:15:57 +0100

    Create integrity_checks.py

---
+++ integrity_checks.py
@@ -0,0 +1,27 @@
+### INTEGRITÄTSALGORITHMEN ###
+
+# !/usr/bin/python
+import os
+import os.path
+import hashlib
+
+def hashfile(file):
+    BLOCK_SIZE = 65536
+    file_hash = hashlib.sha256()
+    with open(file, 'rb') as f:
+        fb = f.read(BLOCK_SIZE)
+        while len(fb) > 0:
+            file_hash.update(fb)
+            fb = f.read(BLOCK_SIZE)
+    return file_hash.hexdigest()
+
+def INTEGRITY_ALGORITHM_MD5(src, target):
+    md5_source = hashfile(src)
+    md5_target = hashfile(target)
+    return md5_source == md5_target
+
+def INTEGRITY_ALGORITHM_FILESIZE(src, target):
+    return os.stat(src).st_size == os.stat(target).st_size
+
+def INTEGRITY_ALGORITHM_MODIFICATION_DATE(src, target):
+    return os.path.getmtime(src) == os.path.getmtime(target)
